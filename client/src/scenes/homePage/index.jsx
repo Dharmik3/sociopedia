a6563e560331476e2d746ffbc1017736942d7600
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2023-01-25 23:00:58 +0530

    friend list and sponser

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreen = useMediaQuery("(min-width:1000px");
@@ -28,9 +30,13 @@ const HomePage = () => {
           mt={isNonMobileScreen ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
-          <PostsWidget userId={_id}/>
+          <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreen && <Box flexBasis="26%">
+          <AdvertWidget />
+          <Box m="2rem 0" />
+          <FriendListWidget userId={_id}/>
+        </Box>}
       </Box>
     </Box>
   );
